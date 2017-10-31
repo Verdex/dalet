9e559d023c00cd7f28bf178cc754c6725c650906
David L. Inman <davidlevere@gmail.com>
2017-10-30 21:05:56 -0400

    lexer stub

---
+++ Lexer.cs
@@ -0,0 +1,10 @@
+
+// the idea will be to parse as little as possible given the current lexing context
+// will probably need some sort of lexet or something class to contain sub lexers
+
+namespace Dalet
+{
+    public class Lexer
+    {
+    }
+}
