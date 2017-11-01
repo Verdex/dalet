5716e91c3a4867c009a5cce308c78339ccb83fd1
David L. Inman <davidlevere@gmail.com>
2017-10-31 23:24:49 -0400

    token class

--- Lexer.cs
+++ Lexer.cs
@@ -1,9 +1,23 @@
 
+using System.Collections.Generic;
 // the idea will be to parse as little as possible given the current lexing context
 // will probably need some sort of lexet or something class to contain sub lexers
 
 namespace Dalet
 {
+    public enum TType { Int, String }
+
+    public class Token
+    {
+        public IEnumerable<string> Values { get; }
+        public TType Type { get; }
+        public Token( TType t, params string[] values )
+        {
+            Type = t;
+            Values = values; 
+        }
+    }
+
     public class Lexer
     {
     }
