77d71d359b3f544f013d1dc7ecc389ce5ca0a14f
David L. Inman <davidlevere@gmail.com>
2017-11-02 21:45:20 -0400

    inc prog

--- Util.cs
+++ Util.cs
@@ -11,17 +11,44 @@ namespace Dalet.Util
         {
             Text = Lines( text ).ToList();
         }
+        
+        public string Error( int index, string message )
+        {
+            return Error( index, index, message );
+        }
+
+        public string Error( int start, int end, string message )
+        { 
+            // TODO last line no \n issue
+            foreach( var t in Text )
+            {
+                if ( start > t.Length - 1 )
+                {
+                    start = start - t.Length;
+                    end = end - t.Length;
+                }
+                else if ( t[t.Length - 1] != '\n' && t[t.Length - 1] != '\r' )
+                {
+                    return t + "\n" + new string( '-', start ) + new string( '^', 1 + end - start );
+                }
+                else
+                {
+                    return t + new string( '-', start ) + new string( '^', 1 + end - start );
+                }
+            }
+            return "";
+        }
 
         private static IEnumerable<string> Lines( string t )
         {
             var line = UntilEndLine( t ).ToArray(); 
-            yield return new string( line ).Replace("\t", "    ");
+            yield return new string( line );
 
             var next = t.Substring( line.Length );
             while( next.Length != 0 )
             {
                 line = UntilEndLine( next ).ToArray();
-                yield return new string( line ).Replace("\t", "    ");
+                yield return new string( line );
                 next = next.Substring( line.Length );
             }
         }
